cdaff39b5427eff8d4ef26e2d2c227027316878f
Sebastian Zazzi <sebastian@zazzi.se>
2021-04-20 21:41:00 +0200

    added endpoints for profiles

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 6969;
 const postRouter = require("./routers/postRouter");
+const profileRouter = require("./routers/profileRouter");
 
 const uri =
   "mongodb+srv://admin:admin@twatter.j7drj.mongodb.net/Twatter?retryWrites=true&w=majority";
@@ -11,9 +12,9 @@ mongoose
     const app = express();
     app.use(express.json());
     app.use("/api", postRouter);
+    app.use("/api", profileRouter);
 
     app.listen(port, () =>
       console.log(`Server is running on http://localhost:${port}`)
     );
   });
-
