d2f8f4eb82c9f0e8e71a147379cf3d3e5226280b
supertramps <alexander.liljedahl@live.se>
2021-04-20 14:41:30 +0200

    Database setup

---
+++ server/models/Profile.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const Profile = new mongoose.Schema({
+  userName: { type: String, trim: true },
+  password: { type: String },
+  role: { type: String },
+  name: { type: String, trim: true },
+});
+
+module.exports = mongoose.model("Profile", Profile);
