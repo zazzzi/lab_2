855b08f962d47a5f69f6a8dc830c1734cd93ff61
Intradastingly <asaf_lackgren@hotmail.com>
2021-04-20 11:22:17 +0200

    env file added and localhost cosnole

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 6969;
 
 // console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Server is running on http://localhost:${port}/express_backend`));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
