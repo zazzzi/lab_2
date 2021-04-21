27b87b0f1754000f3c6a29ec9d119c4dd996e76c
supertramps <alexander.liljedahl@live.se>
2021-04-21 14:00:01 +0200

    Moved around some code for readability

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-const Profile = new mongoose.Schema({
+const profileSchema = new mongoose.Schema({
   userName: { type: String, trim: true },
-  password: { type: String },
+  password: hashedPassword,
   role: { type: String },
   name: { type: String, trim: true },
 });
 
-module.exports = mongoose.model("Profile", Profile);
+module.exports = mongoose.model("Profile", profileSchema);
--- server/routers/profileRouter.js
+++ server/routers/profileRouter.js
@@ -1,47 +1,37 @@
-const Profile = require("../models/Profile");
+const profileModel = require("../models/Profile");
 const express = require("express");
-const Post = require("../models/Post");
-const bcrypt = require('bcrypt');
+const postModel = require("../models/Post");
+
 const profileRouter = express.Router();
 
 //get all profiles
 profileRouter.get("/profiles", async (req, res) => {
-  console.log(req.session)
-  const profile = await Profile.find();
+  console.log(req.session);
+  const profile = await profileModel.find();
   res.send(profile);
 });
 
 //post new user/profile
 profileRouter.post("/profiles", async (req, res) => {
-
-  const hashedPassword = await bcrypt.hash(req.body.password, 10)
-
-  const profile = new Profile({
-    userName: req.body.userName,
-    password: hashedPassword,
-    role: req.body.role,
-    name: req.body.name,
-  });
-  
-  await profile.save();
-  res.send(profile);
+  const profile = await profileModel.create(req.body);
+  res.status(201).json(profile);
 });
 
 //get specific user from ID
 profileRouter.get("/profiles/:id", async (req, res) => {
-  const profile = await Profile.findOne({ _id: req.params.id });
+  const profile = await profileModel.findOne({ _id: req.params.id });
   res.send(profile);
 });
 
 //delete user from id
 profileRouter.delete("/profiles/:id", async (req, res) => {
-  const profile = await Profile.findOneAndDelete({ _id: req.params.id });
+  const profile = await profileModel.findOneAndDelete({ _id: req.params.id });
   res.send(profile);
 });
 
 //update user from id (non admin command)
 profileRouter.put("/profiles/:id", async (req, res) => {
-  const profile = await Post.findOneAndUpdate(
+  const profile = await postModel.findOneAndUpdate(
     { _id: req.params.id },
     {
       $set: {
@@ -50,11 +40,10 @@ profileRouter.put("/profiles/:id", async (req, res) => {
       },
     },
     { new: true },
-    (err, doc) => {
+    (err) => {
       if (err) {
         console.log("OH NO SOMETHING WENT WRONG");
       }
-      console.log(doc);
     }
   );
   res.send(profile);
